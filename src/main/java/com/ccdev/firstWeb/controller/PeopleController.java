351e7e689bea1b512385b823709d6016330042b2
CrisCanonDev <criscanon21@gmail.com>
2024-03-04 16:07:00 +0200

    Creating @Controller stereotype, adding thymeleaf depedency

---
+++ src/main/java/com/ccdev/firstWeb/controller/PeopleController.java
@@ -0,0 +1,27 @@
+package com.ccdev.firstWeb.controller;
+
+
+import com.ccdev.firstWeb.entities.People;
+import com.ccdev.firstWeb.repository.PeopleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/people")
+public class PeopleController {
+    @Autowired
+    private PeopleRepository peopleRepository;
+
+    @GetMapping
+    public String listPeople(Model model){
+        List<People> people = peopleRepository.findAll();
+        model.addAttribute("listPeople", people);
+        return "list";
+    }
+
+}
