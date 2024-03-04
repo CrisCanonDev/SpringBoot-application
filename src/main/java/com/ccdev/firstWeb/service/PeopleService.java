fe6da367ea3adfce526e6fa888a5fdd3e02cbc49
CrisCanonDev <criscanon21@gmail.com>
2024-03-04 15:18:26 +0200

    Creating service stereotype, data inyection from @repository

---
+++ src/main/java/com/ccdev/firstWeb/service/PeopleService.java
@@ -0,0 +1,13 @@
+package com.ccdev.firstWeb.service;
+
+import com.ccdev.firstWeb.entities.People;
+import java.util.List;
+
+public interface PeopleService {
+    List<People> obtainAll();
+    People obtainById(Long id);
+    People createPerson(People person);
+    People updatePerson(Long id, People person);
+    void deletePerson(Long id);
+
+}
---
+++ src/main/java/com/ccdev/firstWeb/service/impl/PeopleServiceImpl.java
@@ -0,0 +1,44 @@
+package com.ccdev.firstWeb.service.impl;
+
+import com.ccdev.firstWeb.entities.People;
+import com.ccdev.firstWeb.repository.PeopleRepository;
+import com.ccdev.firstWeb.service.PeopleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class PeopleServiceImpl implements PeopleService {
+    @Autowired
+    private PeopleRepository peopleRepository;
+    @Override
+    public List<People> obtainAll() {
+        return peopleRepository.findAll();
+    }
+
+    @Override
+    public People obtainById(Long id) {
+        return peopleRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public People createPerson(People person) {
+        return peopleRepository.save(person);
+    }
+
+    @Override
+    public People updatePerson(Long id, People person) {
+        People personDDBB = peopleRepository.findById(id).orElse(null);
+        if(personDDBB != null){
+            personDDBB.setName(person.getName());
+            personDDBB.setAge(person.getAge());
+            return personDDBB;
+        }
+        return null;
+    }
+
+    @Override
+    public void deletePerson(Long id) {
+        peopleRepository.deleteById(id);
+    }
+}
