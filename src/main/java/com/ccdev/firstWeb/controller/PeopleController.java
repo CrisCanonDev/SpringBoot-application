93a0e129cb577fca3349b3f620a06249c55db0a7
CrisCanonDev <criscanon21@gmail.com>
2024-03-04 17:04:04 +0200

    Adding view to show form that add a person, adding functionalty to reditect to form view and adding person to the @service

--- src/main/java/com/ccdev/firstWeb/controller/PeopleController.java
+++ src/main/java/com/ccdev/firstWeb/controller/PeopleController.java
@@ -3,10 +3,13 @@ package com.ccdev.firstWeb.controller;
 
 import com.ccdev.firstWeb.entities.People;
 import com.ccdev.firstWeb.repository.PeopleRepository;
+import com.ccdev.firstWeb.service.PeopleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -15,11 +18,11 @@ import java.util.List;
 @RequestMapping("/people")
 public class PeopleController {
     @Autowired
-    private PeopleRepository peopleRepository;
+    private PeopleService peopleService;
 
     @GetMapping
     public String listPeople(Model model){
-        List<People> people = peopleRepository.findAll();
+        List<People> people = peopleService.obtainAll();
         model.addAttribute("listPeople", people);
         return "list";
     }
@@ -27,7 +30,14 @@ public class PeopleController {
     @GetMapping("/new")
     public String showForm(Model model){
         model.addAttribute("person",new People());
+        model.addAttribute("action","/people/new");
         return "form";
+    }
+
+    @PostMapping("/new")
+    public String addPerson(@ModelAttribute People person){
+        peopleService.createPerson(person);
+        return "redirect:/personas";
     }
 
 }
