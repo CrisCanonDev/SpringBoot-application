f97f2b67ac94d7610501d2ecf9a846dd3ba39e32
CrisCanonDev <criscanon21@gmail.com>
2024-03-04 13:47:28 +0200

    first commit

---
+++ src/test/java/com/ccdev/firstWeb/FirstWebSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ccdev.firstWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstWebSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
