d7d07c0eca92aa5797ea2b4f73ed05744eb8030a
CrisCanonDev <criscanon21@gmail.com>
2024-03-15 16:29:23 +0200

    Authorize http request depending on roles

--- src/main/java/com/ccdev/firstWeb/security/WebSecurityConfiguration.java
+++ src/main/java/com/ccdev/firstWeb/security/WebSecurityConfiguration.java
@@ -1,11 +1,15 @@
 package com.ccdev.firstWeb.security;
 
 import org.apache.catalina.UserDatabase;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityFilterAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class WebSecurityConfiguration {
@@ -23,5 +27,17 @@ public class WebSecurityConfiguration {
                 .roles("ADMIN")
                 .build();
         return new InMemoryUserDetailsManager(user1, user2);
+    }
+
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests(
+                auth -> auth
+                        .requestMatchers("/people").permitAll()
+                        .requestMatchers("/people/new").hasAnyRole("ADMIN")
+                        .requestMatchers("/people/delete/*", "people/edit/*").hasRole("ADMIN")
+                        .anyRequest().authenticated() )
+                .httpBasic(Customizer.withDefaults())
+                .exceptionHandling(ex -> ex.accessDeniedPage("/403"));
+        return httpSecurity.build();
     }
 }
